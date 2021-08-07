94ab6b7b7f36ea71f814620345b00a920c176039
The Shadow Scott <smpendland@outlook.com>
2021-08-07 19:39:55 -0400

    Weapon Balancing

--- HorrorHouse/Weapons.cs
+++ HorrorHouse/Weapons.cs
@@ -31,14 +31,14 @@ class Weapon
         ConsoleWriteBlue("1: Claymore - Heavy hitter, but strongly debuffs your swiftness");
         ConsoleWriteInlineGreen("MAX DMG: +5\t"); Console.Write("CRIT: +0%\t"); ConsoleWriteInlineRed("STEALTH: -10%\t"); Console.Write("MAX HEALTH: +0");
         Linefeed(); Linefeed();
-        ConsoleWriteBlue("2: Dagger - Small, fast, and lethal, but makes you squishy");
-        ConsoleWriteInlineRed("MAX DMG: -2\t"); ConsoleWriteInlineGreen("CRIT: +50%\t"); ConsoleWriteInlineGreen("STEALTH: +15%\t"); ConsoleWriteInlineRed("MAX HEALTH: -10");
+        ConsoleWriteBlue("2: Dagger - Small, fast, and lethal, but doesn't hurt that much");
+        ConsoleWriteInlineRed("MAX DMG: -2\t"); ConsoleWriteInlineGreen("CRIT: +15%\t"); ConsoleWriteInlineGreen("STEALTH: +10%\t"); Console.WriteLine("MAX HEALTH: 0");
         Linefeed(); Linefeed();
         ConsoleWriteBlue("3: Rapier - Well rounded, but not extravagant weapon");
-        ConsoleWriteInlineGreen("MAX DMG: +2\t"); ConsoleWriteInlineGreen("CRIT: +10%\t"); ConsoleWriteInlineGreen("STEALTH: +5%\t"); Console.Write("MAX HEALTH: +0");
+        ConsoleWriteInlineGreen("MAX DMG: +2\t"); ConsoleWriteInlineGreen("CRIT: +5%\t"); ConsoleWriteInlineGreen("STEALTH: +5%\t"); Console.Write("MAX HEALTH: +0");
         Linefeed(); Linefeed();
         ConsoleWriteBlue("4: Spelltome - A magic tome that moderately increases your life force");
-        Console.Write("MAX DMG: +0\t"); ConsoleWriteInlineGreen("CRIT: +5%\t"); ConsoleWriteInlineGreen("STEALTH: +5%\t"); ConsoleWriteInlineGreen("MAX HEALTH: +25");
+        Console.Write("MAX DMG: +0\t"); ConsoleWriteInlineGreen("CRIT: +5%\t"); ConsoleWriteInlineGreen("STEALTH: +5%\t"); ConsoleWriteInlineGreen("MAX HEALTH: +20");
         Linefeed(); Linefeed();
         int returnFeed;
         do
@@ -63,14 +63,14 @@ class Weapon
     public class Dagger
     {
         public readonly int maxDamage = 8;
-        public readonly double critRate = 0.35;
-        public readonly double stealthDebuff = -0.15;
-        public readonly int healthBoost = -10;
+        public readonly double critRate = 0.7;
+        public readonly double stealthDebuff = -0.10;
+        public readonly int healthBoost = 0;
     }
     public class Rapier
     {
         public readonly int maxDamage = 12;
-        public readonly double critRate = 0.75;
+        public readonly double critRate = 0.8;
         public readonly double stealthDebuff = -0.05;
         public readonly int healthBoost = 0;
     }
